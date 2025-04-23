516ae2085ead5664856ca2956c654a6d4018e039
marilia pereira <carlos.sousa115@etec.sp.gov.br>
2025-04-22 22:21:24 -0300

    popoo

--- Cliente.cs
+++ Cliente.cs
@@ -7,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace FilaBanco
 {
-    internal class Cliente
-    {
-        public string nome;
+    
+        public class cliente
+        {
+            public int id; { get; set; }
+            public string name { get; set; }
+            public int idade { get; set; }
+            oioi
+
+        }
+        public void Cadastrar()
+        {
+            Console.WriteLine("Digite o nome");
+            this.nome = Console.ReadLine();
+            Console.WriteLine("Digite sua idade");
+            this.idade = int.Parse(Console.ReadLine());
+        }
     }
 }
